b3502907ec8569397f58bbbfa98b91de6e6948fe
Akash <akash.deep@skillnetinc.com>
2018-09-22 20:18:29 +0530

    Spring Boot Gradle Example with Tests

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/greeting")
     public String index(){
         return "Greeting from Spring Boot!";
     }
+
 }
